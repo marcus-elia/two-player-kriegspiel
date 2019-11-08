11e28e1d3cc0421d6eb25c170b756982833c2069
marcus-elia <mselia@uvm.edu>
2019-11-08 18:28:56 -0500

    Filled in getAttackableLocations

--- src/Rook.java
+++ src/Rook.java
@@ -11,8 +11,58 @@ public class Rook extends Piece
     }
 
     @Override
+    // Check in each of the 4 directions until you find another piece, or reach and edge
     public ArrayList<Integer> getAttackableLocations()
     {
-        return null;
+        ArrayList<Integer> locs = new ArrayList<Integer>();
+        int[] coords = locationToCoords(this.location);
+        int x = coords[0];
+        int y = coords[1];
+
+        int tempX = x - 1;
+        // Check left
+        while(tempX > -1)
+        {
+            locs.add(coordsToLocation(tempX, y));
+            if(!this.board.isEmpty(tempX, y))
+            {
+                break;
+            }
+            tempX--;
+        }
+        // Check right
+        tempX = x + 1;
+        while(tempX < 8)
+        {
+            locs.add(coordsToLocation(tempX, y));
+            if(!this.board.isEmpty(tempX, y))
+            {
+                break;
+            }
+            tempX++;
+        }
+        // Check up
+        int tempY = y - 1;
+        while(tempY > -1)
+        {
+            locs.add(coordsToLocation(x, tempY));
+            if(!this.board.isEmpty(x, tempY))
+            {
+                break;
+            }
+            tempY--;
+        }
+        // Check down
+        tempY = y + 1;
+        while(tempY < 8)
+        {
+            locs.add(coordsToLocation(x, tempY));
+            if(!this.board.isEmpty(x, tempY))
+            {
+                break;
+            }
+            tempY++;
+        }
+        return locs;
     }
 }
