cc2213c2da2f74441cd337aa592f9ab9bfb30c87
marcus-elia <mselia@uvm.edu>
2019-11-08 18:09:45 -0500

    Filled in getReachableLocations

--- src/Knight.java
+++ src/Knight.java
@@ -13,6 +13,56 @@ public class Knight extends Piece
     @Override
     public ArrayList<Integer> getReachableLocations()
     {
-        return null;
+        ArrayList<Integer> locs = new ArrayList<Integer>();
+        int[] coords = locationToCoords(this.location);
+        int x = coords[0];
+        int y = coords[1];
+
+        // Check if the 8 positions are in bounds
+        if(x > 1)
+        {
+            if(y > 0)
+            {
+                locs.add(coordsToLocation(x-2, y-1));
+            }
+            if(y < 7)
+            {
+                locs.add(coordsToLocation(x-2, y+1));
+            }
+        }
+        if(x > 0)
+        {
+            if(y > 1)
+            {
+                locs.add(coordsToLocation(x-1, y-2));
+            }
+            if(y < 6)
+            {
+                locs.add(coordsToLocation(x-1, y+2));
+            }
+        }
+        if(x < 6)
+        {
+            if(y > 0)
+            {
+                locs.add(coordsToLocation(x+2, y-1));
+            }
+            if(y < 7)
+            {
+                locs.add(coordsToLocation(x+2, y+1));
+            }
+        }
+        if(x < 7)
+        {
+            if(y > 1)
+            {
+                locs.add(coordsToLocation(x+1, y-2));
+            }
+            if(y < 6)
+            {
+                locs.add(coordsToLocation(x+1, y+2));
+            }
+        }
+        return locs;
     }
 }
