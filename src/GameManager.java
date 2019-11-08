902f6777deb9c9036935b8206ef7493cef6dbd35
marcus-elia <mselia@uvm.edu>
2019-11-08 09:35:47 -0500

    Added windowHeight and windowWidth variables and corresponding getters

--- src/GameManager.java
+++ src/GameManager.java
@@ -3,10 +3,14 @@ import java.awt.*;
 public class GameManager
 {
     private ChessBoard board;
+    private int windowWidth;
+    private int windowHeight;
 
-    public GameManager()
+    public GameManager(int width, int height)
     {
         this.board = new ChessBoard();
+        this.windowWidth = width;
+        this.windowHeight = height;
     }
 
     public void tick()
@@ -17,5 +21,23 @@ public class GameManager
     public void render(Graphics2D g2d)
     {
         this.board.render(g2d);
+    }
+
+    // ===============================
+    //
+    //           Getters
+    //
+    // ===============================
+    public ChessBoard getBoard()
+    {
+        return this.board;
+    }
+    public int getWindowWidth()
+    {
+        return this.windowWidth;
+    }
+    public int getWindowHeight()
+    {
+        return this.windowHeight;
     }
 }
