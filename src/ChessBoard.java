db45cd75bb8818a53f4ac4fc0d87f652cbe15838
marcus-elia <mselia@uvm.edu>
2019-11-07 20:40:31 -0500

    The ChessBoard constructor creates an empty 2D array of Pieces

---
+++ src/ChessBoard.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class ChessBoard
+{
+
+    private Piece[][] pieces;
+
+    public ChessBoard()
+    {
+        pieces = new Piece[8][8];
+    }
+
+    public void tick()
+    {
+
+    }
+
+    public void render(Graphics2D g2d)
+    {
+
+    }
+}
