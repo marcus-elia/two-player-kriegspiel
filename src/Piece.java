39453a8a77fc7435ba8d8b5078219a828191641d
marcus-elia <mselia@uvm.edu>
2019-11-07 20:41:03 -0500

    The Piece class is abstract

---
+++ src/Piece.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public abstract class Piece
+{
+    public Piece()
+    {
+
+    }
+
+    public void tick()
+    {
+
+    }
+
+    public void render(Graphics2D g2d)
+    {
+
+    }
+}
