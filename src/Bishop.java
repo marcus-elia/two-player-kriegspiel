a46153f545b75a8de0471656bfe20688d5ad51f8
marcus-elia <mselia@uvm.edu>
2019-11-08 18:35:47 -0500

    Filled in getAttackableLocations

--- src/Bishop.java
+++ src/Bishop.java
@@ -11,8 +11,66 @@ public class Bishop extends Piece
     }
 
     @Override
+    // Check along each of the four diagonals until you go out of bounds or hit a piece
     public ArrayList<Integer> getAttackableLocations()
     {
-        return null;
+        ArrayList<Integer> locs = new ArrayList<Integer>();
+        int[] coords = locationToCoords(this.location);
+        int x = coords[0];
+        int y = coords[1];
+
+        // check up and left
+        int tempX = x - 1;
+        int tempY = y - 1;
+        while(tempX > -1 && tempY > -1)
+        {
+            locs.add(coordsToLocation(tempX, tempY));
+            if(!this.board.isEmpty(tempX, tempY))
+            {
+                break;
+            }
+            tempX--;
+            tempY--;
+        }
+        // check up and right
+        tempX = x + 1;
+        tempY = y - 1;
+        while(tempX < 8 && tempY > -1)
+        {
+            locs.add(coordsToLocation(tempX, tempY));
+            if(!this.board.isEmpty(tempX, tempY))
+            {
+                break;
+            }
+            tempX++;
+            tempY--;
+        }
+        // check down and left
+        tempX = x - 1;
+        tempY = y + 1;
+        while(tempX > -1 && tempY < 8)
+        {
+            locs.add(coordsToLocation(tempX, tempY));
+            if(!this.board.isEmpty(tempX, tempY))
+            {
+                break;
+            }
+            tempX--;
+            tempY++;
+        }
+        // check down and right
+        tempX = x + 1;
+        tempY = y + 1;
+        while(tempX > 8 && tempY > 8)
+        {
+            locs.add(coordsToLocation(tempX, tempY));
+            if(!this.board.isEmpty(tempX, tempY))
+            {
+                break;
+            }
+            tempX++;
+            tempY++;
+        }
+        return locs;
     }
 }
